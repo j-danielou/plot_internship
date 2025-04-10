980b11c512df098dfe1b66bcbd37348f22a35f23
j-danielou <jules.dan@hotmail.fr>
2025-04-10 11:55:56 +0200

    add plotting for 6 regions

--- R/plot_ts_bran.R
+++ R/plot_ts_bran.R
@@ -1,6 +1,8 @@
-source("function_plot_ts.R")
-oisst = read.table(file = "C:/Users/jdanielou/Desktop/plots_internship/ts_csv/oisst/oisst_mhw.csv")
-bran = read.table(file = "C:/Users/jdanielou/Desktop/plots_internship/ts_csv/bran/bran_mhw.csv")
+source("R/function_plot_ts.R")
+
+#------------------ El Niño Area ----------------
+oisst = read.table(file = "C:/Users/jdanielou/Desktop/plot_internship/ts_csv/oisst/oisst_mhw.csv")
+bran = read.table(file = "C:/Users/jdanielou/Desktop/plot_internship/ts_csv/bran/bran_mhw.csv")
 
 oisst=oisst$V1
 bran = bran$V1
@@ -10,5 +12,41 @@ x11(width = 18, height = 12)
 plot_diff(time = time, x = bran, y = oisst, Title ="Time-Series of OISST and BRAN20 (1993-01 / 2021-12)", 
           Legend = c("SST BRAN2020", "SST OISST", "Difference (BRAN - OISST)"))
 
-dev.copy(jpeg,file="C:/Users/jdanielou/Desktop/plots_internship/plot/ts_oisst_bran_mhw.jpeg", width = 15.5, height = 10, units = "in", res = 300)
+dev.copy(png,file="C:/Users/jdanielou/Desktop/plots_internship/plot/ts_oisst_bran_mhw.png", width = 15.5, height = 10, units = "in", res = 300)
 dev.off()
+
+
+#------------------ 6 zones -----------------
+# Définir les régions
+regions <- c("nw", "nc", "ne", "sw", "sc", "se")
+
+# Fonctions de lecture
+read_ts <- function(dataset, region) {
+  path <- paste0("C:/Users/jdanielou/Desktop/plot_internship/ts_csv/", dataset, "/", dataset, "_", region, ".csv")
+  ts <- read.table(file = path)$V1
+  return(ts)
+}
+
+# Lire tous les fichiers en liste nommée
+oisst_list <- lapply(regions, function(r) read_ts("oisst", r))
+names(oisst_list) <- regions
+
+bran_list <- lapply(regions, function(r) read_ts("bran", r))
+names(bran_list) <- regions
+
+# Définir le temps (en supposant que toutes les séries ont la même longueur)
+time <- 1:length(bran_list[[1]])
+
+# Plot
+for (r in regions) {
+  x11(width = 18, height = 12)
+  plot_diff(time = time,
+            x = bran_list[[r]],
+            y = oisst_list[[r]],
+            Title = paste0("Time-Series of OISST and BRAN20 (1993-01 / 2021-12), (", toupper(r), ")"),
+            Legend = c("SST BRAN2020", "SST OISST", "Difference (BRAN - OISST)"))
+  
+  dev.copy(png,file=paste0("C:/Users/jdanielou/Desktop/plots_internship/plot/plots_bran/ts_oisst_bran_",r,".png"), width = 15.5, height = 10, units = "in", res = 300)
+  dev.off()
+}
+
